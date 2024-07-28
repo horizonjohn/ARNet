269099a790cafd99d25f0af5a5863b4d80c2e916
A_John <59439017+horizonjohn@users.noreply.github.com>
2024-07-28 22:23:04 +0800

    update

--- src/train_plus_utils.py
+++ src/train_plus_utils.py
@@ -3,6 +3,8 @@ Acknowledgements:
 1. https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py
 2. https://github.com/IBM/CrossViT
 3. https://github.com/rishikksh20/CrossViT-pytorch
+4. https://github.com/sthalles/SimCLR/blob/master/simclr.py
+5. https://github.com/facebookresearch/moco-v3/blob/main/main_moco.py
 """
 
 import os
